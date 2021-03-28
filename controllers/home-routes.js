83fe3c9c0712696a0d5c353bef2a52820c5fbf22
jpkashlak <jpkashlak@gmail.com>
2021-03-28 18:50:11 -0500

    reworked home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,101 +1,136 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const sequelize = require("../config/connection");
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
     Post.findAll({
-        attributes: [
-            'id',
-            'title',
-            'body',
-            'user_id',
-        ],
-        include: [
-            {
-              model: User,
-              as: "user",
-              attributes: ["username"],
-            },
-            {
-              model: Comment,
-              as: "comments",
-              attributes: ["id", 
-                            "comment_text", 
-                            "user_id"
-                          ],
-            },
-        ],
+        attributes: ["id", "title", "body", "user_id"],
+        include: [{
+            model: User,
+            as: "user",
+            attributes: ["username"],
+        },
+        {
+            model: Comment,
+            as: "comments",
+            attributes: ["id", "comment_text", "user_id"],
+        }
+    ]
+  })
+        .then((dbPostData) => {     
+            if (!dbPostData) {
+                res.status(404).json({ message: "Couldn't find any posts" });
+            return;
+            }
+      const posts = dbPostData.map((post) => post.get({ plain: true })); 
+      res.render("home", { posts, loggedIn: req.session.loggedIn });
     })
-        .then(dbPostData => {
-           
-            const posts = dbPostData.map(post => post.get({ plain: true }))
-            res.render('homepage', { 
-                posts,
-                loggedIn: req.session.loggedIn 
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.get("/viewpost/:id", (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ["id", "title", "body", "user_id"],
+        include: [
+        {
+            model: User,
+            as: "user",
+            attributes: ["username"],
+        },
+        {
+            model: Comment,
+            as: "comments",
+            attributes: ["id", "comment_text", "user_id"],
+            include: [
+        {
+            model: User,
+            as: "user",
+            attributes: ["username"],
+        }]
+      }
+    ]
+  })
+        .then((dbPostData) => {  
+            if (!dbPostData) {
+                res.status(404).json({ message: "No Posts Available" });
+            return;
+            }
+            const post = dbPostData.get({ plain: true });
+            const myPost = post.user_id == req.session.user_id;
+            res.render("single-post", {
+                post,
+                loggedIn: req.session.loggedIn,
+                currentUser: myPost,
             });
         })
-        .catch(err => {
+        .catch((err) => {
             console.log(err);
             res.status(500).json(err);
         });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    
-    res.render('login');
-})
 
-router.get('/post/:id', (req, res) => {
-    Post.findOne({
+router.get("/login", (req, res) => {
+    res.render("login", { loggedIn: req.session.loggedIn });
+});
+
+
+router.get("/dashboard", (req, res) => {
+    Post.findAll({
         where: {
-            id: req.params.id
+            user_id: req.session.user_id,
         },
-        attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
+        attributes: ["id", "title", "body", "user_id"],
         include: [
             {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
+            model: User,
+            as: "user",
+            attributes: ["username"]
             },
             {
-                model: User,
-                attributes: ['Username']
+            model: Comment,
+            as: "comments",
+            attributes: ["id", "comment_text", "user_id"],
+            include: [
+                {
+                    model: User,
+                    as: "user",
+                    attributes: ["username"]
+                }
+            ]
             }
-        ]
-    })
-        .then(dbPostData => {
+        ]   
+  })
+        .then((dbPostData) => {
             if (!dbPostData) {
-                res.status(404).json({ message: 'No post found with this id' });
-                return;
+                res.status(404).json({ message: "Couldn't find any posts" });
+            return;
             }
-
-            // serialize the data
-            const post = dbPostData.get({ plain: true });
-
-            // pass data to template
-            res.render('single-post', { 
-                post,
-                loggedIn: req.session.loggedIn 
-            });
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+            res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
         })
-        .catch(err => {
+        .catch((err) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get("/post", (req, res) => {
+    res.render("create-post", { loggedIn: req.session.loggedIn });
+});
+
+router.get("/edit/:id", (req, res) => {
+    res.render("edit-post", {
+        loggedIn: req.session.loggedIn,
+        post_id: req.params.id,
+    });
 });
 
 module.exports = router;
